bcbf0368f5c9d664972052579fffc56ab0e70c21
Débora Limas <113652801+dbrlimas@users.noreply.github.com>
2023-02-08 21:24:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const atualizar = (tempo) => {
     const horas = document.getElementById('horas');
     const dias = document.getElementById('dias');
 
-     //em quantidadeMinutos foi feita uma operação para multiplicar os segundos e os minutos e depois essa quantidade foi dividida por 60 para converter os segundos em minutos. O objeto "Math.floor" foi utilizado para usar somente números inteiros. Já em quantidadeHoras a operação é para retirar os dias (por isso é multiplicado por 24, que é a quantidade de horas de um dia). Assim, o programa retira os dias do tempo dado e resta somente as horas( em segundos, por isso é multiplicado novamente os minutos por segundos). Para finalizar, em quantidadeDias, o tempo é dividido pela quantidade de dias que tem um segundo. //
+     /* Em quantidadeMinutos foi feita uma operação para multiplicar os segundos e os minutos e depois essa quantidade foi dividida por 60 para converter os segundos em minutos. 
+     O objeto "Math.floor" foi utilizado para usar somente números inteiros. Já em quantidadeHoras a operação é para retirar os dias (por isso é multiplicado por 24, que é a quantidade de horas de um dia). Assim, o programa retira os dias do tempo dado e resta somente as horas( em segundos, por isso é multiplicado novamente os minutos por segundos). 
+     Para finalizar, em quantidadeDias, o tempo é dividido pela quantidade de dias que tem um segundo. */
 
     const quantidadeSegundos = tempo % 60;
     const quantidadeMinutos = Math.floor((tempo % (60 * 60)) / 60);
